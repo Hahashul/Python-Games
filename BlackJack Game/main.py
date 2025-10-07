0b0b27ee7d9d29aecb60cfa06c7a4196adbf1f18
Hahashul <harshuladwani@gmail.com>
2025-10-08 00:48:38 +0530

    Added the money system in Blackjack

--- BlackJack Game/main.py
+++ BlackJack Game/main.py
@@ -15,7 +15,6 @@
 
 import random
 from art import logo
-
 # Function to deal a random card from the deck
 def deal_card():
     """Returns a random card from the deck"""
@@ -35,15 +34,56 @@ def calculate_score(cards):
 
     return sum(cards)
 
+def ask_bet(user,bet):
+    
+    bet=int(input("Place your bet-: "))
+    bet_loop= True
+    while bet_loop:
+        if user-bet<0:
+            print("Insufficient Balance,Try Again")
+            bet=int(input("Place your bet-: "))
+        else:
+            bet_loop=False
+    return bet         
+
+def win_bet(u_score, c_score):
+    if u_score == c_score:
+        return 0
+    elif c_score == 0:
+        return 1
+    elif u_score == 0:
+        return 2
+    elif u_score > 21:
+        return 1
+    elif c_score > 21:
+        return 2
+    elif u_score > c_score:
+        return 2
+    else:
+        return 1
+
+def calculate_bet(win_bet,bet,user):
+    if win_bet==1:
+        user=user-bet
+        return user
+    elif win_bet==2:
+        user=user+bet
+        return user
+    else:
+        return user        
+
+
 # Function to compare the final scores and determine the outcome
+user_money=1000
+
 def compare(u_score, c_score):
     """Compares the user score u_score against the computer score c_score."""
     if u_score == c_score:
-        return "Draw 🙃"
+        return "Draw 🙃" 
     elif c_score == 0:
-        return "Lose, opponent has Blackjack 😱"
+        return "Lose, opponent has Blackjack 😱" 
     elif u_score == 0:
-        return "Win with a Blackjack 😎"
+        return "Win with a Blackjack 😎" 
     elif u_score > 21:
         return "You went over. You lose 😭"
     elif c_score > 21:
@@ -54,7 +94,8 @@ def compare(u_score, c_score):
         return "You lose 😤"
 
 # Main function to play the game
-def play_game():
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    print("\n" * 20)
     print(logo)
     user_cards = []
     computer_cards = []
@@ -65,11 +106,16 @@ def play_game():
     for _ in range(2):
         user_cards.append(deal_card())
         computer_cards.append(deal_card())
+
+    print(f"Current Balance -: {user_money}")
+    bet=0
+    bet=ask_bet(user_money,bet)
+    
     
     while not is_game_over:
         user_score = calculate_score(user_cards)                         # Calculate the user's score
         computer_score = calculate_score(computer_cards)                  # Calculate the computer's score
-        print(f"Your cards: {user_cards}, current score: {user_score}")
+        print(f"\nYour cards: {user_cards}, current score: {user_score}")
         print(f"Computer's first card: {computer_cards[0]}")
 # Check for a blackjack or if the user has exceeded 21
         if user_score == 0 or computer_score == 0 or user_score > 21:
@@ -85,14 +131,17 @@ def play_game():
         computer_cards.append(deal_card())
         computer_score = calculate_score(computer_cards)
 # Final results
-    print(f"Your final hand: {user_cards}, final score: {user_score}")
+    print(f"\nYour final hand: {user_cards}, final score: {user_score}")
     print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
     print(compare(user_score, computer_score))
+    check_win=win_bet(user_score,computer_score)
+    user_money=calculate_bet(check_win,bet,user_money)
+    print(f"\n\nCurrent Balance -: {user_money}")
 
-# Ask the user if they want to play a game of Blackjack
-while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
-    print("\n" * 20)
-    play_game()
+    if user_money==0:
+        print("You lost it all!!!")
+        break
+        
 
 print("Thank you for playing!")     
 # End of the game
